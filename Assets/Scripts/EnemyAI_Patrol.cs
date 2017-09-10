44694b0bd90eb1f4b4e1fd074fb69fd003de1db5
RvBVakama <RvBVakama@users.noreply.github.com>
2017-09-11 09:48:27 +1000

    v .03

---
+++ Assets/Scripts/EnemyAI_Patrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_Patrol : MonoBehaviour
+{
+    public Transform[] arrWaypoints;
+    public float fSpeed;
+    public int nCurWaypoint;
+    public bool bPatroling = true;
+    public Vector3 v3Target;
+    public Vector3 v3MoveDir;
+    public Vector3 v3Vel;
+
+    private Animator Anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nCurWaypoint < arrWaypoints.Length)
+        {
+            v3Target = arrWaypoints[nCurWaypoint].position;
+            v3MoveDir = v3Target - transform.position;
+            v3Vel = GetComponent<Rigidbody>().velocity;
+            float fMagnitude = v3Vel.magnitude;
+
+            if (fMagnitude > 0)
+            {
+                Anim.SetFloat("Monster", 1.0f);
+            }
+            else
+            {
+                Anim.SetFloat("Monster", 0.0f);
+            }
+            
+            if (v3MoveDir.magnitude < 1)
+            {
+                nCurWaypoint++;
+            }
+            else
+            {
+                v3Vel = v3MoveDir.normalized * fSpeed;
+            }
+        }
+        else
+        {
+            if (bPatroling)
+            {
+                nCurWaypoint = 0;
+            }
+            else
+            {
+                v3Vel = Vector3.zero;
+            }
+        }
+        GetComponent<Rigidbody>().velocity = v3Vel;
+        transform.LookAt(v3Target);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    bool isOnGround = true;
+
+    private Rigidbody Rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+
+        float xx = Input.GetAxis("Horizontal");
+        float zz = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        //Vector3 v3VelPost = Rb.velocity;
+        Vector3 v3VelPost = Rb.velocity;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rb.velocity = Rb.velocity + gameObject.transform.forward * Time.deltaTime * 300000;
+            Debug.Log(Rb.velocity);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Rb.velocity = Rb.velocity + (gameObject.transform.forward * -1) * 3;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            //Debug.DrawLine(transform.position + Vector3.up, transform.position + Vector3.down * 5);
+            if (isOnGround == true)
+            {
+                //transform.Translate(transform.up * 200 * Time.deltaTime, Space.World);
+                Vector3 v3VelPos = Rb.velocity;
+                Rb.velocity = new Vector3(v3VelPos.x, 15, v3VelPos.z);
+                isOnGround = false;
+            }
+        }
+
+        //Rb.angularVelocity = Vector3.zero;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ground");
+        isOnGround = true;
+    }
+}
